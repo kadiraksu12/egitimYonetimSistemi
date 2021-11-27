d9d18dac370ec32b89668d0a351350a5da7cca9a
levent <levent.kalay@hotmail.com>
2021-11-27 20:47:19 +0300

    Initial version of egitimkadir generated by generator-jhipster@7.3.1

---
+++ src/main/java/com/yesevi/egitimkadir/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.yesevi.egitimkadir.web.rest.vm;
---
+++ src/test/java/com/yesevi/egitimkadir/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.yesevi.egitimkadir;
+
+import com.yesevi.egitimkadir.EgitimkadirApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = EgitimkadirApp.class)
+public @interface IntegrationTest {
+}
